bfc0208bd31cece8a75c9464d1b84aa89dd39547
muhammad2603 <fattahillahmuhammad48@gmail.com>
2024-10-18 11:11:50 +0700

    Added EJS

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 
 const PORT = 3000
 
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
 app.get('/', (req, res) => {
 
-    res.json({ message: "Hello, World!" })
-
+    res.render('index', { title: "Home SiMAMI" })
+    
 })
 
 app.listen(PORT, () => {
