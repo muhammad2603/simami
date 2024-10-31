1891ad23499cb035169365e8b6f076fba6a138a4
muhammad2603 <fattahillahmuhammad48@gmail.com>
2024-10-31 10:27:17 +0700

    Menambahkan Fitur Login.

---
+++ public/js/login.js
@@ -0,0 +1,52 @@
+$(() => {
+    // Variable Button Login
+    const btnLogin = $('#btnLogin'),
+          errorLogin = $("#errorLogin")
+    // Counter Login
+    let waitingForNextLogin = 0
+    // Button Link Sign Up Clicked
+    $("#btnLinkSignUp").click(() => window.location.href = '/sign-up')
+    // Button Login Clicked
+    btnLogin.click(async function() {
+        // Set Waiting For Login
+        setWaitingForLogin(waitingForNextLogin, btnLogin)
+        // Data Username and Password User
+        const data = {
+            username: $('#username').val().trim(),
+            password: $('#password').val().trim()
+        }
+        // Try & Catch
+        try {
+            // Response from Server
+            const resp = await requestAjax({
+                url: "/login",
+                type: "POST",
+                contentType: "application/json",
+                processData: false,
+                data: JSON.stringify(data)
+            })
+            // If Error Login Message Exist, setErrorLoginMessage
+            if( errorLogin.text() ) setErrorLoginMessage(errorLogin, '')
+            // Message from Response (Object)
+            const { message } = resp
+            // Set Text on Button Login
+            btnLogin.text(`✔️ ${message}`)
+        }catch(err) {
+            // If error
+            // Increase Counter Login with up +1
+            waitingForNextLogin += 1
+            // Get Status on Response Text
+            const { status, message } = JSON.parse(err.responseText)
+            // If Status is 404, then return with show message error to element errorLogin
+            if( status === 404 ) setErrorLoginMessage(errorLogin, '* Pengguna tidak ditemukan.')
+            // If Status is 400, then return with show message error to element errorLogin
+            if( status === 400 ) setErrorLoginMessage(errorLogin, `* ${message}`)
+            // Set Text on Button Login
+            btnLogin.text(`❌ Login Gagal!`)
+            // Set Text on Button Login to default 1 second next
+            setTimeout(() => btnLogin.text('Login'), 1000)
+        }
+
+    })
+
+})
